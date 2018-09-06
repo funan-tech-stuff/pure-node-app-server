28a855103c82c9ac1db6bc4188025a8524263ad5
Dylan Kiew <dylanksl@yahoo.com>
2018-09-06 21:40:46 +0800

    Commit #1002 - Parsing HTTP Method

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ const server = http.createServer((req, res)=>{
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+    // get the http method (get, post, put)
+    const method = req.method.toLowerCase();
+
     // send a response
     res.end('Hello World\n');
 
     // log the request path
-    console.log(`Request received on path: ${trimmedPath}`);
+    console.log(`Request received on path: ${trimmedPath} with method : ${method}`);
     
 });
 
