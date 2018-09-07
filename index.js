efa8db8d0b121ad1f0f1e8f50735ec82cd58a811
Dylan Kiew <dylanksl@yahoo.com>
2018-09-07 10:01:00 +0800

    Commit #1004 - Parsing Headers

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ const server = http.createServer((req, res)=>{
     // get the http method (get, post, put)
     const method = req.method.toLowerCase();
 
+    // get the headers as an object
+    const headers = req.headers;
+
     // send a response
     res.end('Hello World\n');
 
     // log the request path
     console.log(`Request received on path: ${trimmedPath} with method : ${method} and with these query string parameters `, queryStringObject);
+    console.log(`Request received with these headers : `, headers);
     
 });
 
