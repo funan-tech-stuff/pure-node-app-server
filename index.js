71a4f68f2f916dd9865254d139aa09942353c99e
Dylan Kiew <dylanksl@yahoo.com>
2018-09-07 10:12:48 +0800

    Commit #1005 - Parsing Payload (a.k.a body)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 
 // server should responsd to all reqests with a string
@@ -28,13 +29,32 @@ const server = http.createServer((req, res)=>{
     // get the headers as an object
     const headers = req.headers;
 
-    // send a response
-    res.end('Hello World\n');
+    // get the payload, if any.
+    // payload comes into the http server as streams.
+    // so it is important to handle the bites of data coming through and form it into the full payload.
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
 
-    // log the request path
-    console.log(`Request received on path: ${trimmedPath} with method : ${method} and with these query string parameters `, queryStringObject);
-    console.log(`Request received with these headers : `, headers);
-    
+    // event listener for stream data coming into the http server
+    req.on('data', (data)=>{
+        
+        // accumulate the data stream content
+        buffer += decoder.write(data);
+    })
+
+    // event listener for the end of streaming.
+    // this event will always be called, even if there are no payload pasted into the http server.
+    req.on('end', ()=>{
+        buffer += decoder.end();
+        // send a response
+        res.end('Hello World\n');
+
+        // log the request path
+        console.log(`Request received on path: ${trimmedPath} with method : ${method} and with these query string parameters `, queryStringObject);
+        console.log(`Request received with these headers : `, headers);
+        console.log(`Request received with this payload : `, buffer);
+    });
+
 });
 
 // start the server, and have it listen on port 3000
